ed4fa7f5901b3e8baf259203006a0431af5ac0ee
Drazen Mrvos <dmrvos@gmail.com>
2020-04-22 17:08:50 +0200

    IS4 example with working google auth

---
+++ server/IdentityUtils.Demos.Google.IS4/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityUtils.Demos.Google.IS4.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
